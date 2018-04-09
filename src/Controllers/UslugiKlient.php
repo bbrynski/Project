a87973d79832cd8841f79fca6bd36dbfe67e1947
ddzieran <36821198+ddzieran@users.noreply.github.com>
2018-04-09 14:46:33 +0200

    UslugiKlientMVC

---
+++ src/Controllers/UslugiKlient.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Damian
+ * Date: 2018-03-20
+ * Time: 10:28
+ */
+
+namespace Controllers;
+
+
+class UslugiKlient extends Controller{
+
+        public function getAll(){
+            $view =$this ->getView('UslugiKlient');
+              $data = null;
+             if(\Tools\Session::is('message'))
+                 $data['message'] = \Tools\Session::get('message');
+            if(\Tools\Session::is('error'))
+                $data['error'] = \Tools\Session::get('error');
+            $view->getAll($data);
+            \Tools\Session::clear('message');
+            \Tools\Session::clear('error');
+        }
+
+
+
+
+        public function addform(){
+            $view = $this->getView('UslugiKlient');
+            $view->addform();
+        }
+
+        public function add()
+        {
+            $model = $this->getModel('UslugiKlient');
+            $data = $model->add($_POST['Id_Model'],$_POST['Id_Uslugi'],$_POST['Id_Klient'],$_POST['Opis']);
+            if(isset($data['error']))
+                \Tools\Session::set('error', $data['error']);
+            if(isset($data['message']))
+                \Tools\Session::set('message', $data['message']);
+            $this->redirect('UslugiKlient');
+        }
+
+    public function delete($id){
+
+        $model=$this->getModel('UslugiKlient');
+        $data = $model->delete($id);
+        if(isset($data['error']))
+            \Tools\Session::set('error', $data['error']);
+        if(isset($data['message']))
+            \Tools\Session::set('message', $data['message']);
+        $this->redirect('UslugiKlient/');
+    }
+    public function editform($id){
+
+        $model=$this->getModel('UslugiKlient');
+        $data=$model->getOne($id);
+        if(isset($data['error'])){
+            \Tools\Session::set('error', $data['error']);
+            $this->redirect('UslugiKlient/');
+        }
+        $view = $this->getView('UslugiKlient');
+        $view->editform($data['uslugi'][0]);
+    }
+    public function update(){
+
+        $model=$this->getModel('UslugiKlient');
+        $data = $model->update($_POST['id'], $_POST['Id_Model'],$_POST['Id_Uslugi'],$_POST['Id_Klient'],$_POST['Opis']);
+
+        if(isset($data['error']))
+            \Tools\Session::set('error', $data['error']);
+        if(isset($data['message']))
+            \Tools\Session::set('message', $data['message']);
+        $this->redirect('UslugiKlient/');
+    }
+}
---
+++ src/Views/UslugiKlient.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Damian
+ * Date: 2018-03-22
+ * Time: 21:08
+ */
+
+namespace Views;
+
+
+class UslugiKlient extends View
+{
+    public function getAll($data = null){
+        if(isset($data['message']))
+            $this->set('message',$data['message']);
+        if(isset($data['error']))
+            $this->set('error',$data['error']);
+        $model = $this->getModel('UslugiKlient');
+        $data = $model->getAll();
+        $this->set('uslugi', $data['uslugi']);
+        if(isset($data['error']))
+            $this->set('error', $data['error']);
+        
+        $this->set('customScript', array('datatables.min','table'));
+
+        $model = $this->getModel('Samochod');
+        $data = $model->getAll();
+        $this->set('samochody', $data['samochody']);
+
+        $model = $this->getModel('Uslugi');
+        $data = $model->getAll();
+        $this->set('uslugi', $data['uslugi']);
+
+
+        $model = $this->getModel('Klient');
+        $data = $model->getAll();
+        $this->set('klienci', $data['klienci']);
+       
+    
+
+
+        
+        
+        $this->render('UslugiKlientGetAll');
+    }
+
+
+
+
+
+
+    public function addform(){
+
+        $model = $this->getModel('Samochod');
+        $data = $model->getAll();
+        $this->set('samochody', $data['samochody']);
+
+        $model = $this->getModel('Uslugi');
+        $data = $model->getAll();
+        $this->set('uslugi', $data['uslugi']);
+
+
+        $model = $this->getModel('Klient');
+        $data = $model->getAll();
+        $this->set('klienci', $data['klienci']);
+
+
+
+        $this->render('UslugiKlientAddForm');
+    }
+
+    public function editform($UslugiKlient){
+        $this->set('id', $UslugiKlient[\Config\Database\DBConfig\UslugiKlient::$id]);
+        $this->set('Id_Model', $UslugiKlient[\Config\Database\DBConfig\UslugiKlient::$Id_Model]);
+        $this->set('Id_Uslugi', $UslugiKlient[\Config\Database\DBConfig\UslugiKlient::$Id_Uslugi]);
+        $this->set('Id_Klient', $UslugiKlient[\Config\Database\DBConfig\UslugiKlient::$Id_Klient]);
+        $this->set('opis', $UslugiKlient[\Config\Database\DBConfig\UslugiKlient::$opis]);
+
+
+        $this->set('Modele', $this->getModel('Samochod')->getAll()['samochody']);
+        $this->set('uslugi', $this->getModel('Uslugi')->getAll()['uslugi']);
+        $this->set('$klienci', $this->getModel('Klient')->getAll()['$klienci']);
+
+
+        $this->set('customScript','UslugiKlient');
+        $this->render('UslugiKlientEditForm');
+    }
+
+}
